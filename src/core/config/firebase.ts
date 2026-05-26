5e9aed6edb81511989e06dd2b9cba05ad1d741fa
Juan Antonio <musasime4@gmail.com>
2026-05-26 12:34:14 +0200

    fix: estructura inicial y firebase.ts

--- src/core/config/firebase.ts
+++ src/core/config/firebase.ts
@@ -1,25 +1,25 @@
 import { initializeApp, getApps, getApp } from 'firebase/app';
-import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
-import AsyncStorage from '@react-native-async-storage/async-storage';
 
-// Reemplaza esto con las credenciales que te da la consola de Firebase
+import { getFirestore } from 'firebase/firestore';
+
+import { getAuth } from 'firebase/auth';
+
 const firebaseConfig = {
-  apiKey: "TU_API_KEY",
-  authDomain: "TU_AUTH_DOMAIN",
-  projectId: "TU_PROJECT_ID",
-  storageBucket: "TU_STORAGE_BUCKET",
-  messagingSenderId: "TU_MESSAGING_SENDER_ID",
-  appId: "TU_APP_ID"
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId:
+    process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
 };
 
-// Inicializar Firebase evitando duplicados en el Hot Reload de Expo
-const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
+const app =
+  getApps().length === 0
+    ? initializeApp(firebaseConfig)
+    : getApp();
 
-// Configurar la autenticación para que recuerde la sesión del opositor al cerrar la app
-const auth = initializeAuth(app, {
-  persistence: getReactNativePersistence(AsyncStorage),
-});
+const auth = getAuth(app);
 
 const db = getFirestore(app);
 
