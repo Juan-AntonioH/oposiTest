3aea96e41d853d9141cb2bfea232e43800bcfb09
Juan Antonio <musasime4@gmail.com>
2026-06-03 10:36:44 +0200

    Toolbar con ajustamiento a la pantalla.

--- src/features/dashboard/screens/DashboardScreen.tsx
+++ src/features/dashboard/screens/DashboardScreen.tsx
@@ -1,14 +1,26 @@
-import { View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 import { Toolbar } from '@/shared/components/toolbar/ToolBar';
+import { styles } from './DashboardScreen.styles';
 
 export function DashboardScreen() {
   return (
-    <View style={{ flex: 1 }}>
+    // SafeAreaView evita que el contenido se meta debajo de:
+    // - barra de estado (hora, batería, etc)
+    // - notch (cámara frontal)
+    // - gestos inferiores del sistema
+    <SafeAreaView style={styles.container}>
+      
+      {/* Toolbar fijo en todas las pantallas */}
+      {/* Aquí solo cambia su configuración (título, botones, etc.) */}
       <Toolbar
         title="OposiTest"
         onMenuPress={() => alert('Menu')}
       />
-    </View>
+
+      {/* Aquí irá el contenido específico de la pantalla */}
+      {/* Ej: listas, cards, estadísticas, etc. */}
+      
+    </SafeAreaView>
   );
 }
